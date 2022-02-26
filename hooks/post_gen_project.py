3f7f2d129fe9774e8e70e2b0d0e0f6ce5ab89364
Tom LeZotte <tom@lezotte.net>
2022-02-26 16:25:24 -0600

    switched post to os.system

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import os
-import subprocess
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -10,15 +9,15 @@ def remove_file(filepath):
 
 
 if __name__ == '__main__':
-    subprocess.run(["make", "install"])
+    os.system("make install")
 
     if '{{ cookiecutter.project_type }}' == 'api':
-        subprocess.run(["make", "api"])
+        os.system("make api")
 
     if '{{ cookiecutter.project_type }}' == 'cli':
-        subprocess.run(["make", "cli"])
+        os.system("make cli")
 
-    subprocess.run(["make", "git"])
-    subprocess.run(["make", "show"])
+    os.system("make git")
+    os.system("make show")
 
     remove_file("README.rst")
