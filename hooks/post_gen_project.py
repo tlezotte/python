dfe67a2eacd3fa18ceae95f1d63b80e08661bf51
Tom LeZotte <tom@lezotte.net>
2022-02-28 17:00:33 -0600

    moved to functions

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,36 +6,43 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def remove_file(filepath):
+    """
+    Remove the file if it exists
+    """
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+def git_config(item):
+    """
+    Get a git user config value
+    """
+    config = subprocess.run(['git', 'config', '--global', 'user.' + item], stdout=subprocess.PIPE)
+    results = config.stdout.decode('utf-8').strip()
+    return results
+
+def change_git_config(filename):
+    """
+    Change the git config user's name and email in the specified file
+    """
+    fin = open(filename, "rt")
+    data = fin.read()
+    data = data.replace('FULL_NAME', git_config("name"))
+    data = data.replace('EMAIL', git_config("email"))
+    fin.close()
+
+    fin = open(filename, "wt")
+    fin.write(data)
+    fin.close()
+
+def init_project():
+    """
+    Initialize a new project
+    """
+    os.system("make init")
 
 
 if __name__ == '__main__':
-    name = subprocess.run(['git', 'config', '--global', 'user.name'], stdout=subprocess.PIPE)
-    user_name = name.stdout.decode('utf-8').strip()
-    email = subprocess.run(['git', 'config', '--global', 'user.email'], stdout=subprocess.PIPE)
-    user_email = email.stdout.decode('utf-8').strip()
+    files = ["pyproject.toml","Makefile"]
+    for file in files:
+        change_git_config(file)
 
-    # Replace git name and email in pyproject.toml
-    fin = open("pyproject.toml", "rt")
-    data = fin.read()
-    data = data.replace('FULL_NAME', user_name)
-    data = data.replace('EMAIL', user_email)
-    fin.close()
-
-    fin = open("pyproject.toml", "wt")
-    fin.write(data)
-    fin.close()
-
-    # Replace git name and email in Makefile
-    fin = open("Makefile", "rt")
-    data = fin.read()
-    data = data.replace('FULL_NAME', user_name)
-    data = data.replace('EMAIL', user_email)
-    fin.close()
-
-    fin = open("Makefile", "wt")
-    fin.write(data)
-    fin.close()
-
-    # Init make build
-    os.system("make init")
+    init_project()
