584839503b2ad6cd1638b7d907fb45292bb9a670
Tom LeZotte <tom@lezotte.net>
2022-03-01 13:22:26 -0600

    add docstrings

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,22 +6,31 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def remove_file(filepath):
-    """
-    Remove the file if it exists
+    """Remove the file if it exists
+
+    Args:
+        filepath (string): filename to remove
     """
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 def git_config(item):
-    """
-    Get a git user config value
+    """Get git global user config
+
+    Args:
+        item (string): user item to get
+
+    Returns:
+        string: value of user item
     """
     config = subprocess.run(['git', 'config', '--global', 'user.' + item], stdout=subprocess.PIPE)
     results = config.stdout.decode('utf-8').strip()
     return results
 
 def change_git_config(filename):
-    """
-    Change the git config user's name and email in the specified file
+    """Change the git config user's name and email in the specified file
+
+    Args:
+        filename (string): name of file to change
     """
     fin = open(filename, "rt")
     data = fin.read()
@@ -34,13 +43,15 @@ def change_git_config(filename):
     fin.close()
 
 def run_make(target):
-    """
-    Initialize a new project
+    """Initialize a new project
+
+    Args:
+        target (string): name of make target to run
     """
     os.system("make " + target)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
     files = ["pyproject.toml","Makefile"]
     for file in files:
         change_git_config(file)
