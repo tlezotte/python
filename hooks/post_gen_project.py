2ecf279e2b27c07e47ba8b4e272ab52357d8d197
Tom LeZotte <tom@lezotte.net>
2022-03-04 21:06:27 -0600

    create tests and project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 import os
-import shutil
 
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
-PROJECT_PATH = os.path.join(PROJECT_DIRECTORY, "../{{cookiecutter.project_slug}}")
 
 
 def run_make(target):
@@ -15,16 +13,6 @@ def run_make(target):
     """
     os.system("make " + target)
 
-def copy_files():
-    shutil.copyfile(PROJECT_DIRECTORY + "/.gitignore", PROJECT_DIRECTORY)
-    shutil.copyfile(PROJECT_DIRECTORY + "/Makefile", PROJECT_DIRECTORY)
-    shutil.copyfile(PROJECT_DIRECTORY + "/README.md", PROJECT_DIRECTORY)
-
-def remove_tmp():
-    shutil.rmtree(PROJECT_DIRECTORY)
-
 
 if __name__ == '__main__':
-    copy_files()
-    remove_tmp()
     run_make("init")
