a48f0fa942bae088c25346f8267e7db3981ce709
Tom LeZotte <tom@lezotte.net>
2022-03-04 19:48:25 -0600

    dot dot

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,8 +4,7 @@ import shutil
 
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
-PROJECT_PATH = os.path.join(PROJECT_DIRECTORY, "{{cookiecutter.project_slug}}")
-PROJECT_TMP_PATH = os.path.join(PROJECT_DIRECTORY, "{{cookiecutter.project_slug}}_tmp")
+PROJECT_PATH = os.path.join(PROJECT_DIRECTORY, "../{{cookiecutter.project_slug}}")
 
 
 def run_make(target):
@@ -17,12 +16,12 @@ def run_make(target):
     os.system("make " + target)
 
 def copy_files():
-    shutil.copyfile(PROJECT_TMP_PATH + "/.gitignore", PROJECT_PATH)
-    shutil.copyfile(PROJECT_TMP_PATH + "/Makefile", PROJECT_PATH)
-    shutil.copyfile(PROJECT_TMP_PATH + "/README.md", PROJECT_PATH)
+    shutil.copyfile(PROJECT_DIRECTORY + "/.gitignore", PROJECT_DIRECTORY)
+    shutil.copyfile(PROJECT_DIRECTORY + "/Makefile", PROJECT_DIRECTORY)
+    shutil.copyfile(PROJECT_DIRECTORY + "/README.md", PROJECT_DIRECTORY)
 
 def remove_tmp():
-    shutil.rmtree(PROJECT_TMP_PATH)
+    shutil.rmtree(PROJECT_DIRECTORY)
 
 
 if __name__ == '__main__':
